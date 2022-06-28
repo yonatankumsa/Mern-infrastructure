75b3047830b5b0d1bd24ac7b6aa872284f6332f0
yonatankumsa <yonatankumsa99@gmail.com>
2022-06-28 11:19:20 -0400

    mern infrastructure

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -5,21 +5,22 @@ import NewOrderPage from "../NewOrderPage/NewOrderPage";
 import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
 import { Routes, Route } from "react-router-dom";
 import NavBar from "../../components/NavBar/NavBar";
+import { getUser } from '../../utilities/users-service';
 
 export default function App() {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(getUser());
   return (
     <main className="App">
       {user ? (
         <>
-          <NavBar />
+          <NavBar user={user}/>
           <Routes>
             <Route path="/orders/new" element={<NewOrderPage />} />
             <Route path="/orders" element={<OrderHistoryPage />} />
           </Routes>
         </>
       ) : (
-        <AuthPage />
+        <AuthPage setUser={ setUser }/>
       )}
     </main>
   );
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,7 +1,12 @@
-export default function AuthPage() {
+import SignUpForm from "../../components/SignUpForm/SignUpForm";
+
+export default function AuthPage(props) {
    
     return (
+        <main>
         <h1>AuthPage</h1>
+       < SignUpForm setUser={props.setUser}/>
+       </main>
     );
   }
   
